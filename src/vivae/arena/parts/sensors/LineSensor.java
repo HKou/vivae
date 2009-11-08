a7023f4cd943a9f504ebf106b9f06fe2d2df0b76
koutnij <koutnij@x41.felk.cvut.cz>
2009-11-08 22:28:18 +0100

    sensors moved to special package updated classloader (canload classes out of vivae package) PointSensor still does not work

---
+++ src/vivae/arena/parts/sensors/LineSensor.java
@@ -0,0 +1,11 @@
+package vivae.arena.parts.sensors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koutnij
+ * Date: Nov 8, 2009
+ * Time: 8:24:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LineSensor {
+}
