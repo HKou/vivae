f24c1cf99fe811b24caaca686dd95ad17b453ee6
koutnij <koutnij@x41.felk.cvut.cz>
2009-11-08 22:24:40 +0100

    sensors moved to special package updated classloader (canload classes out of vivae package) PointSensor still does not work

---
+++ src/vivae/arena/parts/sensors/DistanceSensor.java
@@ -0,0 +1,11 @@
+package vivae.arena.parts.sensors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koutnij
+ * Date: Nov 8, 2009
+ * Time: 10:24:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DistanceSensor {
+}
