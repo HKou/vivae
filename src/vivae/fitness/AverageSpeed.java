9f9820a15c708c7e7c0468ef6cb52dae04b05bd0
koutnij <koutnij@x41.felk.cvut.cz>
2009-11-09 21:54:32 +0100

    fitness functions

---
+++ src/vivae/fitness/AverageSpeed.java
@@ -0,0 +1,34 @@
+package vivae.fitness;
+
+import vivae.arena.Arena;
+import vivae.arena.parts.Active;
+import vivae.example.FRNNController;
+import vivae.example.FRNNControlledRobot;
+import vivae.util.Util;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koutnij
+ * Date: Nov 9, 2009
+ * Time: 9:18:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AverageSpeed extends FitnessFunction{
+    Arena arena;
+    public AverageSpeed(Arena arena){
+        this.arena = arena;
+    }
+
+    public double getFitness(){
+        double res=0d;
+        Vector<Active> actives = arena.getActives();
+        for (Iterator<Active> it = actives.iterator(); it.hasNext();) {
+            Active ag = it.next();
+            res+=ag.odometer;
+        }
+        return res/actives.size()/arena.stepsDone;
+     }
+}
---
+++ src/vivae/fitness/FitnessFunction.java
@@ -0,0 +1,14 @@
+package vivae.fitness;
+
+import vivae.arena.Arena;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koutnij
+ * Date: Nov 9, 2009
+ * Time: 9:20:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class FitnessFunction {
+    public abstract double getFitness();
+}
